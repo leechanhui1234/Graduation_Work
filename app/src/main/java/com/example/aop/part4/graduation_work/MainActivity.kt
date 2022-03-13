4fe403b68b355fd3ae0cdbb46fe4b4a327262fea
leechanhui1234 <yepp0517@naver.com>
2022-03-13 16:24:51 +0900

    simple_code

--- app/src/main/java/com/example/aop/part4/graduation_work/MainActivity.kt
+++ app/src/main/java/com/example/aop/part4/graduation_work/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.aop.part4.graduation_work
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private val test: TextView by lazy{
+        findViewById<TextView>(R.id.test)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        test.text = "테스트"
     }
 }
