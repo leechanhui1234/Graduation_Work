df353949dc21f877a0353a44dc6b47d5b4faaa4a
TTprotocol <wnsdud7941@naver.com>
2022-03-19 20:18:38 +0900

    버튼 및 화면 색상 변경

--- app/src/main/java/com/example/aop/part4/graduation_work/Request/LoginRequest.kt
+++ app/src/main/java/com/example/aop/part4/graduation_work/Request/LoginRequest.kt
@@ -5,17 +5,17 @@ import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.example.aop.part4.graduation_work.Url.Url
 
-class LoginRequest(
-    userId: String,
-    userPw: String,
-    listener: Response.Listener<String>?
-): StringRequest(
+class LoginRequest (
+    userId : String,
+    userPw : String,
+    listener : Response.Listener<String>?
+) : StringRequest (
     Method.POST, Url.loginUrl, listener, null
 ) {
-    private val map: MutableMap<String, String>
+    private val map : MutableMap<String, String>
 
     @Throws(AuthFailureError::class)
-    override fun getParams(): Map<String, String>? {
+    override fun getParams() : Map<String, String>? {
         return map
     }
     init {
--- app/src/main/java/com/example/aop/part4/graduation_work/data/UserData.kt
+++ app/src/main/java/com/example/aop/part4/graduation_work/data/UserData.kt
@@ -4,11 +4,11 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class UserData(
-    val userId: String,
-    val userPw: String,
-    val userEmail: String,
-    val userAge: Int,
-    val userValue: String,
-    val userName: String
-): Parcelable
+data class UserData (
+    val userId : String,
+    val userPw : String,
+    val userEmail : String,
+    val userAge : Int,
+    val userValue : String,
+    val userName : String
+) : Parcelable
