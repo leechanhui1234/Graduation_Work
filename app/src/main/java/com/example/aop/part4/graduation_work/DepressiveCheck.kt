52792be70d04a0ea5d630211a18d34901d3c3358
이상혁 <tkdgur4404@gmail.com>
2022-04-23 17:11:10 +0900

    depression_check_add and button add

--- app/src/main/java/com/example/aop/part4/graduation_work/DepressiveCheck.kt
+++ app/src/main/java/com/example/aop/part4/graduation_work/DepressiveCheck.kt
@@ -8,9 +8,31 @@ import com.example.aop.part4.graduation_work.databinding.DepressiveCheckBinding
 class DepressiveCheck : AppCompatActivity(){
     private lateinit var binding: DepressiveCheckBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        val intent1 = Intent(this, MainPage::class.java)
+        val intent2 = Intent(this, Ces::class.java)
+        val intent3 = Intent(this, Depression::class.java)
+        val intent4 = Intent(this, Old::class.java)
         super.onCreate(savedInstanceState)
         binding = DepressiveCheckBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        with(binding){
+            backbtn.setOnClickListener {
+                startActivity(intent1)
+                finish()
+            }
+            cesCheck.setOnClickListener{
+                startActivity(intent2)
+                finish()
+            }
+            depCheck.setOnClickListener{
+                startActivity(intent3)
+                finish()
+            }
+            oldCheck.setOnClickListener{
+                startActivity(intent4)
+                finish()
+            }
+        }
 
     }
 
@@ -19,4 +41,5 @@ class DepressiveCheck : AppCompatActivity(){
         startActivity(intent)
         finish()
     }
+
 }
